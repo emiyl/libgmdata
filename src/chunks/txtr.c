b46821292d4b335de96cd121d49f73f3a52b65f2
emiyl <me@emiyl.com>
2026-08-22 21:40:49 +0100

    update txtr

--- src/chunks/txtr.c
+++ src/chunks/txtr.c
@@ -1,18 +1,42 @@
 #include "common.h"
+#include "../datawin.h"
+
+typedef struct {
+    bool hasGeneratedMips;
+    bool has2022_3;
+    bool has2022_9;
+} TextureArgs;
 
 static int TXTR_pointerTable_parse(Reader *reader, DataWin *dw, void *out, void *extraData) {
-    (void)extraData; (void)dw;
+    (void)dw;
     Texture *tex = (Texture *)out;
     memset(tex, 0, sizeof(*tex));
+    TextureArgs *args = (TextureArgs *)extraData;
 
     tex->present = true;
     read(&tex->scaled, UInt32);
-    read(&tex->generatedMips, UInt32);
-    read(&tex->textureBlockSize, UInt32);
-    read(&tex->textureWidth, Int32);
-    read(&tex->textureHeight, Int32);
-    read(&tex->indexInGroup, Int32);
-    
+    if (args->hasGeneratedMips) {
+        read(&tex->generatedMips, UInt32);
+    } else {
+        tex->generatedMips = 0;
+    }
+    if (args->has2022_3) {
+        read(&tex->textureBlockSize, UInt32);
+    } else {
+        tex->textureBlockSize = 0;
+    }
+    if (args->has2022_9) {
+        read(&tex->textureWidth, Int32);
+        read(&tex->textureHeight, Int32);
+        read(&tex->indexInGroup, Int32);
+    } else {
+        tex->textureWidth = 0;
+        tex->textureHeight = 0;
+        tex->indexInGroup = 0;
+    }
+    read(&tex->blobOffset, UInt32);
+    tex->blobData = NULL;
+
     return 0;
 }
 
@@ -35,14 +59,87 @@ int TXTR_parse(DataWin *dw) {
     Reader reader;
     Reader_init(&reader, base, chunk.length, chunk.offset, "TXTR");
 
-    return Reader_readAndParsePointerTable(
+    uint32_t *ptrs;
+    Reader_readPointerTable(&reader, &ptrs, &t->count);
+
+    if (t->count == 0) {
+        free(ptrs);
+        t->textures = NULL;
+        return -1;
+    }
+
+    TextureArgs args = {0};
+
+    // Read metadata entries
+    args.hasGeneratedMips = DataWin_isVersionAtLeast(dw, 2, 0, 0, 0);
+
+    // Detect GMS 2022.3+ (TextureBlockSize field) and 2022.9+ (Width/Height/IndexInGroup fields) by probing the distance between the first two entry pointers.
+    // Only works when there are at least 2 textures (which is almost always the case for real games).
+    // Layouts:
+    //   pre-2022.3: scaled+generatedMips+blobOffset = 12 bytes
+    //   2022.3+: ... + textureBlockSize = 16 bytes
+    //   2022.9+: ... + width + height + indexInGroup = 28 bytes
+    args.has2022_3 = DataWin_isVersionAtLeast(dw, 2022, 3, 0, 0);
+    args.has2022_9 = DataWin_isVersionAtLeast(dw, 2022, 9, 0, 0);
+    if (t->count >= 2 && args.hasGeneratedMips && !args.has2022_9 && ptrs[0] != 0 && ptrs[1] != 0) {
+        uint32_t diff = ptrs[1] - ptrs[0];
+        if (diff == 28) {
+            DataWin_bumpVersionTo(dw, 2022, 9, 0, 0);
+            args.has2022_3 = true;
+            args.has2022_9 = true;
+        } else if (diff == 16 && !args.has2022_3) {
+            DataWin_bumpVersionTo(dw, 2022, 3, 0, 0);
+            args.has2022_3 = true;
+        }
+    }
+
+    if(Reader_parsePointerTable(
         &reader, dw,
-        (void **)&t->textures, NULL,
-        &t->count, sizeof(Texture),
+        ptrs, t->count,
+        (void**)&t->textures, sizeof(Texture),
+        &args,
         TXTR_pointerTable_parse,
         TXTR_pointerTable_missingHandler,
         NULL
-    );
+    )) {
+        free(ptrs);
+        logWarn("TXTR: Failed to parse pointer table");
+        return -1;
+    };
+
+    free(ptrs);
+
+    // Compute blob sizes from successive offsets
+    {
+    repeat(t->count, i) {
+        if (t->textures[i].blobOffset == 0) {
+            t->textures[i].blobSize = 0; // external texture
+            continue;
+        }
+        if (t->count > i + 1 && t->textures[i + 1].blobOffset != 0) {
+            t->textures[i].blobSize = t->textures[i + 1].blobOffset - t->textures[i].blobOffset;
+        } else {
+            uint32_t chunkEnd = chunk.offset + chunk.length;
+            t->textures[i].blobSize = (uint32_t)(chunkEnd - t->textures[i].blobOffset);
+        }
+    }
+    }
+
+    // Load blob data into owned buffers
+    if (!dw->lazyLoadTextures) {
+        repeat(t->count, i) {
+            if (t->textures[i].blobOffset == 0 || t->textures[i].blobSize == 0) continue;
+            if (dw->mappedFile) {
+                t->textures[i].blobData = dw->mappedFile + t->textures[i].blobOffset;
+                t->textures[i].mapped = true;
+            } else {
+                Reader_seek(&reader, t->textures[i].blobOffset);
+                Reader_readBytes(&reader, t->textures[i].blobData, t->textures[i].blobSize);
+            }
+        }
+    }
+
+    return 0;
 }
 
 int TXTR_free(TxtrChunk *t) {
